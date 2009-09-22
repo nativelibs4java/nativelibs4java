c181a4594bbf6832e07725476ea34944716b8fa8
Olivier Chafik <olivier.chafik@gmail.com>
2009-09-22 19:29:58 +0000

    Added many missing math and geom functions

--- libraries/OpenCL/src/main/scala/scalacl/Functions.scala
+++ libraries/OpenCL/src/main/scala/scalacl/Functions.scala
@@ -9,6 +9,12 @@ package scalacl
 
 
 trait Functions {
+  
+  case class Fun2WithPtrAsSecond(nam: String, outTyp: PrimType, arg1: Expr, arg2: Expr, f: (Double, Double) => Double)
+    extends Fun2(nam, outTyp, arg1, arg2, f)
+  {
+    override def toString = name + "(" + arg1 + ", &" + arg2 + ")"
+  }
 
   def die1 = (_: Double) => throw new RuntimeException("Not implemented")
   def die2 = (_: Double, _: Double) => throw new RuntimeException("Not implemented")
@@ -18,34 +24,60 @@ trait Functions {
     (x: Expr) => Fun1(name, DoubleType, x, f)
   private def f2d(name: String, f: (Double, Double) => Double) =
     (x: Expr, y: Expr) => Fun2(name, DoubleType, x, y, f)
+  private def f2dWithPtrAsSecond(name: String) =
+    (x: Expr, y: Expr) => Fun2WithPtrAsSecond(name, DoubleType, x, y, die2)
   private def f3d(name: String, f: (Double, Double, Double) => Double) =
     (x: Expr, y: Expr, z: Expr) => Fun3(name, DoubleType, x, y, z, f)
 
-  var cos = f1d("cos", Math.cos)
   var sin = f1d("sin", Math.sin)
+  var cos = f1d("cos", Math.cos)
   var tan = f1d("tan", Math.tan)
-  var atan = f1d("atan", Math.atan)
-  var acos = f1d("acos", Math.acos)
+
   var asin = f1d("asin", Math.asin)
-  var atanh = f1d("atanh", die1)
-  var acosh = f1d("acosh", die1)
-  var asinh = f1d("asinh", die1)
-  var atanpi = f1d("atanpi", Math.atan(_) / Math.Pi)
-  var acospi = f1d("acospi", Math.acos(_) / Math.Pi)
-  var asinpi = f1d("asinpi", Math.asin(_) / Math.Pi)
-  var cosh = f1d("cosh", die1)
-  var sinh = f1d("sinh", die1)
-  var tanh = f1d("tanh", die1)
+  var acos = f1d("acos", Math.acos)
+  var atan = f1d("atan", Math.atan)
   var atan2 = f2d("atan2", Math.atan2)
+
+  var sinpi = f1d("sinpi", x => Math.sin(x * Math.Pi))
+  var cospi = f1d("cospi", x => Math.cos(x * Math.Pi))
+  var tanpi = f1d("tanpi", x => Math.tan(x * Math.Pi))
+
+  var asinpi = f1d("asinpi", Math.asin(_) / Math.Pi)
+  var acospi = f1d("acospi", Math.acos(_) / Math.Pi)
+  var atanpi = f1d("atanpi", Math.atan(_) / Math.Pi)
   var atan2pi = f2d("atan2pi", Math.atan2(_, _) / Math.Pi)
 
-  /// Cube root
-  var cbrt = f1d("cbrt", x => Math.exp(x * Math.log(1/3.0)))
+  var sinh = f1d("sinh", java.lang.Math.sinh)
+  var cosh = f1d("cosh", java.lang.Math.cosh)
+  var tanh = f1d("tanh", java.lang.Math.tanh)
 
+  var asinh = f1d("asinh", die1)
+  var acosh = f1d("acosh", die1)
+  var atanh = f1d("atanh", die1)
+  
+  var log = f1d("log", Math.log)
+  var logb = f1d("logb", die1)
+  var log2 = f1d("log2", Math.log(_) / Math.log(2))
+  var log10 = f1d("log10", java.lang.Math.log10)
+  var log1p = f1d("log1p", java.lang.Math.log1p)
+  var exp = f1d("exp", Math.exp)
+  var pow = f2d("pow", Math.pow)
+  var pown = f2d("pown", Math.pow)
+  var powr = f2d("powr", Math.pow)
+  var root = f2d("root", (x, y) => Math.exp(x * Math.log(1 / y)))
+
+  /// Cube root
+  var cbrt = f1d("cbrt", java.lang.Math.cbrt)
   /// Square root
   var sqrt = f1d("sqrt", Math.sqrt)
+  /// Inverse square root
+  var rsqrt = f1d("rsqrt", 1 / Math.sqrt(_))
+  /// Gamma function
+  var tgamma = f1d("tgamma", die1)
 
+  var remainder = f2d("remainder", Math.IEEEremainder)
 
+  
   /// |x|
   var abs = f1d("abs", Math.abs)
 
@@ -64,7 +96,7 @@ trait Functions {
   /// Number of leading 0-bits in x
   var clz = f1d("clz", die1)
 
-  ///mul_hi(a, b) + c
+  /// mul_hi(a, b) + c
   var mad_hi = f3d("mad_hi", die3)
 
   /// (Optional) Multiply 24-bit integer values a and b and add the 32-bit integer result to 32-bit integer c
@@ -118,5 +150,45 @@ trait Functions {
 
   /// Sign of x
   var sign = f1d("sign", Math.signum)
+
+  /// Error function
+  var erf = f1d("erf", die1)
+  /// Complementary error function
+  var erfc = f1d("erfc", die1)
+
+
+  /// Round to integer towards +infinity
+  var ceil = f1d("ceil", Math.ceil)
+
+  /// x with sign changed to sign of y
+  var copysign = f2d("copysign", (x, y) => Math.abs(x) * Math.signum(y))
+
+  var floor = f1d("floor", Math.floor)
+  var trunc = f1d("trunc", x => if (x >= 0) x.asInstanceOf[Int] else -((-x).asInstanceOf[Int]))
+
+  var fma = f3d("fma", die3)
+  var fabs = f1d("abs", die1)
+  var ilogb = f1d("ilogb", die1)
+  var ldexp = f2d("ldexp", (x, n) => x * Math.pow(2, n))
+  var hypot = f2d("hypot", (x, y) => Math.sqrt(x * x + y * y))
+
+  var mad = f3d("mad", (a, b, c) => a * b + c)
+  var round = f1d("round", x => Math.round(x))
+  var rint = f1d("rint", die1)
+
+  var sincos = f2dWithPtrAsSecond("sincos")
+  var fract = f2dWithPtrAsSecond("fract")
+  var frexp = f2dWithPtrAsSecond("frexp")
+  var modf = f2dWithPtrAsSecond("modf")
+  var lgamma_r = f2dWithPtrAsSecond("lgamma_r")
+
+  var normalize = f2d("normalize", die2)
+  var cross = f2d("cross", die2)
+  var dot = f2d("dot", die2)
+  var distance = f2d("distance", die2)
+  var length = f2d("length", die2)
+  var fast_distance = f2d("fast_distance", die2)
+  var fast_length = f2d("fast_length", die2)
+  var fast_normalize = f2d("fast_normalize", die2)
 
 }
