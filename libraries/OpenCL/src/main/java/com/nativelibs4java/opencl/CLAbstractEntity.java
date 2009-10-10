cd5fb5c6def322f4dbb2fff331458193e82304cb
Olivier Chafik <olivier.chafik@gmail.com>
2009-10-10 20:59:07 +0000

    - Added CLAbstractEntity.release()

--- libraries/OpenCL/src/main/java/com/nativelibs4java/opencl/CLAbstractEntity.java
+++ libraries/OpenCL/src/main/java/com/nativelibs4java/opencl/CLAbstractEntity.java
@@ -31,14 +31,33 @@ abstract class CLAbstractEntity<T extends PointerType> {
 	CLAbstractEntity(T entity) {
 		this(entity, false);
 	}
+	private boolean nullable;
     CLAbstractEntity(T entity, boolean nullable) {
         if (!nullable && entity == null) {
             throw new IllegalArgumentException("Null OpenCL " + getClass().getSimpleName() + " !");
         }
+		this.nullable = nullable;
         this.entity = entity;
     }
 
-    T get() {
+	/**
+	 * Manual release of the OpenCL resources represented by this object.<br/>
+	 * Note that resources are automatically released by the garbage collector, so in general there's no need to call this method.<br/>
+	 * In an environment with fast allocation/deallocation of large objects, it might be safer to call release() manually, though.<br/>
+	 * Note that release() does not necessarily free the object immediately : OpenCL maintains a reference count for all its objects, and an object released on the Java side might still be pointed to by running kernels or queued operations.
+	 */
+	public synchronized void release() {
+		if (entity == null && !nullable)
+			throw new RuntimeException("This " + getClass().getSimpleName() + " has already been released ! Besides, keep in mind that manual release is not necessary, as it will automatically be done by the garbage collector.");
+
+		clear();
+		entity = null;
+	}
+
+    synchronized T get() {
+		if (entity == null && !nullable)
+			throw new RuntimeException("This " + getClass().getSimpleName() + " has been manually released and can't be used anymore !");
+
         return entity;
     }
 
