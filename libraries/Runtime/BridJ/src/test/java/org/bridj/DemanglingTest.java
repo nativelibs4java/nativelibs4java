e211482d2d165e354d93fb174242cb1ceefc1b53
Rémi Émonet <remi-11-goco@heeere.com>
2010-09-08 19:01:25 +0000

    added some GCC demangling tests + improved test framework for demangling
    Simple demangling tests with function, method, parameters types, pointer type (including aliases in the mangling)

--- libraries/Runtime/BridJ/src/test/java/org/bridj/DemanglingTest.java
+++ libraries/Runtime/BridJ/src/test/java/org/bridj/DemanglingTest.java
@@ -5,11 +5,11 @@
 
 package org.bridj;
 
+import org.bridj.Demangler.TypeRef;
 import org.bridj.Demangler.DemanglingException;
 import org.bridj.Demangler.MemberRef;
 import org.bridj.cpp.GCC4Demangler;
 import org.bridj.cpp.VC9Demangler;
-import static org.bridj.Demangler.MemberRef.*;
 import org.bridj.Demangler.SpecialName;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,7 +23,7 @@ public class DemanglingTest {
 	@Test
 	public void gcc() {
 		demangle(
-			null, 
+                        null,
 			"__Z17testInPlaceSquarePdj", 
 			null, "testInPlaceSquare", 
 			void.class, Pointer.class, int.class
@@ -33,17 +33,26 @@ public class DemanglingTest {
     public void testLongLongBackReference() {
         demangle(
             "?test_add9_long@@YA_J_J00000000@Z",
-            null,
+            "_Z14test_add9_longlllllllll",
             null, "test_add9_long",
             long.class, long.class, long.class, long.class, long.class, long.class, long.class, long.class, long.class, long.class
         );
     }
     @Test
+	public void parameterlessFunction() {
+		demangle(
+			null, // TODO
+			"_Z14test_no_paramsv",
+			null, "test_no_params",
+			null
+		);
+	}
+    @Test
 	public void simpleConstructor() {
 		demangle(
-			"??0Ctest@@QEAA@XZ", 
-			null, 
-			CPPTest.Ctest.class, SpecialName.Constructor, 
+			"??0Ctest@@QEAA@XZ",
+			"_ZN5CtestC1Ev",
+			CPPTest.Ctest.class, SpecialName.Constructor,
 			null
 		);
 	}
@@ -51,7 +60,7 @@ public class DemanglingTest {
     public void methods() {
     	demangle(
 			null, 
-			"__ZN5Ctest7testAddEii", 
+			"_ZN5Ctest7testAddEii", 
 			CPPTest.Ctest.class, "testAdd", 
 			int.class, int.class, int.class
 		);
@@ -60,13 +69,22 @@ public class DemanglingTest {
 
     @Test
 	public void simpleFunctions() {
-		demangle("?sinInt@@YANH@Z", null, null, 
+		demangle("?sinInt@@YANH@Z", "_Z6sinInti", null,
 				"sinInt", 
 				double.class, int.class);
-		demangle("?forwardCall@@YAHP6AHHH@ZHH@Z", null, null, "forwardCall", int.class, Pointer.class, int.class, int.class);
+		demangle("?forwardCall@@YAHP6AHHH@ZHH@Z", "_Z11forwardCallPvii", null, "forwardCall", int.class, Pointer.class, int.class, int.class);
+                // NB: the forwardCall test for gcc is written with a "void*" as first parameter (I could not get the pointer type from the VC6 mangled name
 	}
 
-    private void demangle(String vc9, String gcc4, Class enclosingType, Object memberName, Class returnType, Class... paramTypes) {
+    @Test
+    public void complexPointerTypeParameters() {
+        // TODO VC versions
+        // NB: with gcc, we have no info about the return type (don't know about VC6)
+        demangle(null, "_Z14pointerAliasesPPvS_PS0_PPi", null, "pointerAliases", null, Pointer.class, Pointer.class, Pointer.class, Pointer.class);
+        demangle(null, "_Z14pointerAliasesPPvS_PS0_PPi", null, "pointerAliases", null, "**Void", "*Void", "***Void", "**Integer");
+    }
+
+    private void demangle(String vc9, String gcc4, Class enclosingType, Object memberName, Class returnType, Object... paramTypes) {
         try {
         	if (vc9 != null)
         		checkSymbol(vc9, new VC9Demangler(null, vc9).parseSymbol(), enclosingType, memberName, returnType, paramTypes, null, null);
@@ -78,7 +96,7 @@ public class DemanglingTest {
         }
     }
 
-    private void checkSymbol(String str, MemberRef symbol, Class enclosingType, Object memberName, Class returnType, Class[] paramTypes, Annotation[][] paramAnns, AnnotatedElement element) {
+    private void checkSymbol(String str, MemberRef symbol, Class enclosingType, Object memberName, Class returnType, Object[] paramTypes, Annotation[][] paramAnns, AnnotatedElement element) {
         if (symbol == null)
         		assertTrue("Symbol not successfully parsed \"" + str + "\"", false);
     		if (memberName != null)
@@ -94,7 +112,33 @@ public class DemanglingTest {
         assertEquals("Bad number of parameters", paramTypes.length, nArgs);
 
         for (int iArg = 0; iArg < nArgs; iArg++) {
-            assertTrue("Bad type for " + (iArg + 1) + "th param", symbol.paramTypes[iArg].matches(paramTypes[iArg], paramAnns == null ? null : Demangler.annotations(paramAnns[iArg])));
+            if (paramTypes[iArg] instanceof Class) {
+                assertTrue("Bad type for " + (iArg + 1) + "th param", symbol.paramTypes[iArg].matches((Class) paramTypes[iArg], paramAnns == null ? null : Demangler.annotations(paramAnns[iArg])));
+            } else if (paramTypes[iArg] instanceof String) {
+                String targetType = (String) paramTypes[iArg];
+                TypeRef currentType = symbol.paramTypes[iArg];
+                int count = 0;
+                while (targetType.startsWith("*")) {
+                    assertEquals("For " + (iArg + 1) + "th param, after " + count + " dereferencing, wrong non-pointer type", Demangler.PointerTypeRef.class, currentType.getClass());
+                    targetType = targetType.substring(1);
+                    currentType = ((Demangler.PointerTypeRef) currentType).pointedType;
+                    count++;
+                }
+                Class targetPointedType = null;
+                try {
+                    if (targetType.contains(".")) {
+                        throw new RuntimeException("this code has never been used before, comment this exception and check it");
+                        //targetPointedType = Class.forName(targetType);
+                    } else {
+                        targetPointedType = (Class) Class.forName("java.lang." + targetType).getDeclaredField("TYPE").get(null);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("Check your test for target type " + targetType, e);
+                }
+                assertTrue("For " + (iArg + 1) + "th parameter, after " + count + " dereferencing, wrong final pointed type: expected " + targetType + " got " + currentType.getQualifiedName(new StringBuilder(), true), currentType.matches(targetPointedType, null));
+            } else {
+                assertTrue("Problem in the expression of the test code", false);
+            }
         }
     }
 
