cfdd04e571eda9b2b027b037089ff4ac166ea260
Olivier Chafik <olivier.chafik@gmail.com>
2010-10-03 23:21:15 +0000

    ScalaCLPlugin: added license header to all sources

--- libraries/OpenCL/ScalaCLPlugin/src/main/scala/scalacl/TreeBuilders.scala
+++ libraries/OpenCL/ScalaCLPlugin/src/main/scala/scalacl/TreeBuilders.scala
@@ -1,8 +1,37 @@
+/*
+ * ScalaCL - putting Scala on the GPU with JavaCL / OpenCL
+ * http://scalacl.googlecode.com/
+ *
+ * Copyright (c) 2009-2010, Olivier Chafik (http://ochafik.free.fr/)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Olivier Chafik nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY OLIVIER CHAFIK AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package scalacl
 
 import scala.reflect.generic.{Constants, Names, Trees, Types, Symbols}
 
-//import scala.tools.nsc.CompilationUnits
 import scala.tools.nsc.CompilationUnits
 import scala.tools.nsc.plugins.Plugin
 import scala.tools.nsc.plugins.PluginComponent
@@ -59,9 +88,6 @@ extends MiscMatchers
       )
     ).setType(UnitClass.tpe)
 
-
-  //def intVar(owner: Symbol, n: Name, initValue: Tree) = intValOrVar(owner, MUTABLE, n, initValue)
-  //def intVal(owner: Symbol, n: Name, initValue: Tree) = intValOrVar(owner, 0, n, initValue)
   def whileLoop(owner: Symbol, unit: CompilationUnit, tree: Tree, cond: Tree, body: Tree) = {
     val lab = unit.fresh.newName(body.pos, "while$")
     val labTyp = MethodType(Nil, UnitClass.tpe)
@@ -88,19 +114,6 @@ extends MiscMatchers
       ).setSymbol(labSym)
     }
   }
-  /*def newIntVariable(unit: CompilationUnit, prefix: String, symbolOwner: Symbol, pos: Position, mutable: Boolean), initialValue: Tree = Literal(Constant(0)).setType(IntClass.tpe)) = {
-    val name = unit.fresh.newName(pos, prefix)
-    val sym = symbolOwner.newVariable(pos, name) setInfo IntClass.tpe
-    val definition = (
-      if (mutable)
-        intVar(symbolOwner, name, initialValue)
-      else
-        intVal(symbolOwner, name, initialValue)
-    ).setSymbol(sym)
-    //(name, sym, definition)
-    (() => ident(sym, name), definition)
-  }*/
-
   def newVariable(unit: CompilationUnit, prefix: String, symbolOwner: Symbol, pos: Position, mutable: Boolean, initialValue: Tree) = {
     val tpe = initialValue.tpe
     val name = unit.fresh.newName(pos, prefix)
