99dea38d3d2b1c758134ea417340ef6a803dd5cf
Olivier Chafik <olivier.chafik@gmail.com>
2010-10-06 07:48:59 +0000

    ScalaCLPlugin: added sbt configuration

---
+++ libraries/OpenCL/ScalaCLPlugin/project/build/ScalaCLPluginProject.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+class ScalaCLPluginProject(info: ProjectInfo) extends DefaultProject(info)
+{ 
+  override def compileOptions = super.compileOptions ++
+     compileOptions("-optimise")
+     
+  val nativelibs4javaRepo = "NativeLibs4Java Repository" at "http://nativelibs4java.sourceforge.net/maven/"
+  //val scalacl = compilerPlugin("com.nativelibs4java" % "scalacl-compiler-plugin" % "1.0-SNAPSHOT")
+  
+  //val BuSwing = "com.nativelibs4java" % "buswing_2.8.0" % "0.1.1-SNAPSHOT"
+  val junitInterface = "com.novocode" % "junit-interface" % "0.4" % "test"
+  val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.8.0"
+}
