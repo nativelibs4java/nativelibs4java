04fa1f578a629b2f8df72f030bba7d3694cc3588
Olivier Chafik <olivier.chafik@gmail.com>
2010-12-10 09:18:05 +0000

    Added dummy libc test to make sure the library is correctly resolved

---
+++ libraries/Runtime/BridJ/src/test/java/org/bridj/LibCTest.java
@@ -0,0 +1,30 @@
+package org.bridj;
+
+import org.bridj.ann.*; // annotations such as Library...
+
+
+import java.util.Collections;
+import java.util.Iterator;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+@Library("c")
+@org.bridj.ann.Runtime(CRuntime.class)
+public class LibCTest {
+	static {
+		
+		if ("1".equals(System.getenv("JNA")))
+			com.sun.jna.Native.register("c");
+		else
+			BridJ.register();
+	}
+	public static native double fabs(double x);
+	public static native int getpid();
+	
+	@Test
+	public void testFabs() {
+		assertEquals(10.0, fabs(-10.0), 0.000001);
+	}
+}
+	
+
