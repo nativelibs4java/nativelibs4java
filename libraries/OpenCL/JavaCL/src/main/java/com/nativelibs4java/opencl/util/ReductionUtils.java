e7e9ece47a1047a08be44b17031f138b3813e693
Olivier Chafik <olivier.chafik@gmail.com>
2011-01-06 07:30:19 +0000

    JavaCL: - added Char to ReductionUtils

--- libraries/OpenCL/JavaCL/src/main/java/com/nativelibs4java/opencl/util/ReductionUtils.java
+++ libraries/OpenCL/JavaCL/src/main/java/com/nativelibs4java/opencl/util/ReductionUtils.java
@@ -40,29 +40,32 @@ public class ReductionUtils {
         Max;
     }
     public enum Type {
-        Int, Long, Short, Byte, Double, Float, Half;
+        Int, Char, Long, Short, Byte, Double, Float, Half;
 
         public String toCType() {
             if (this == Byte)
                 return "char";
+            if (this == Char)
+                return "short";
             return name().toLowerCase();
         }
         public static Type fromClass(Class<? extends Number> valueType) {
+            if (valueType == Integer.TYPE || valueType == Integer.class)
+                return Int;
+            if (valueType == java.lang.Long.TYPE || valueType == Long.class)
+                return Long;
+            if (valueType == java.lang.Short.TYPE || valueType == Short.class)
+                return Short;
+            if (valueType == java.lang.Double.TYPE || valueType == Double.class)
+                return Double;
+            if (valueType == java.lang.Float.TYPE || valueType == Float.class)
+                return Float;
+            if (valueType == java.lang.Byte.TYPE || valueType == Byte.class)
+                return Byte;
+            
             if (!valueType.isPrimitive())
                 throw new IllegalArgumentException("Reduction value type is not a primitive: '" + valueType.getName() + "' !");
 
-            if (valueType == Integer.TYPE)
-                return Int;
-            if (valueType == java.lang.Long.TYPE)
-                return Long;
-            if (valueType == java.lang.Short.TYPE)
-                return Short;
-            if (valueType == java.lang.Double.TYPE)
-                return Double;
-            if (valueType == java.lang.Float.TYPE)
-                return Float;
-            if (valueType == java.lang.Byte.TYPE)
-                return Byte;
             throw new IllegalArgumentException("Primitive type not handled: '" + valueType.getName() + "' !");
         }
     }
