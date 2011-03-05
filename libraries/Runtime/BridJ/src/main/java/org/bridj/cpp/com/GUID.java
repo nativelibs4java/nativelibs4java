a1c59dbc3459b225ca4e0d300689f0681827fcbb
Olivier Chafik <olivier.chafik@gmail.com>
2011-03-05 17:02:52 +0000

    fixed compilation

--- libraries/Runtime/BridJ/src/main/java/org/bridj/cpp/com/GUID.java
+++ libraries/Runtime/BridJ/src/main/java/org/bridj/cpp/com/GUID.java
@@ -46,7 +46,7 @@ public class GUID {
     OUR_GUID_ENTRY(CLSID_CDeviceMoniker, 0x4315D437,0x5B8C,0x11d0,0xBD,0x3B,0x00,0xA0,0xC9,0x11,0xCE,0x86)
                              1                2        3      4     5     6    7   8    9    10   11   12
                                               
-    Regexp replace (jEdit) on C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include\uuids.h :
+    Regexp replace (jEdit) on C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include\\uuids.h :
     (?m)OUR_GUID_ENTRY\(\s*(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*,\s*0x(\w+)\s*\)
     "public static final String " + _1 + " = \"" + _2 + "-" + _3 + "-" + _4 + "-" + _5 + _6 + "-" + _7 + _8 + _9 + _10 + _11 + _12
     */
