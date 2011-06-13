5c908508fecd24e728a4489023150e71bd51768d
Olivier Chafik <olivier.chafik@gmail.com>
2011-06-13 12:41:39 +0000

    BridJ: android build scripting

--- libraries/Runtime/BridJ/src/main/cpp/android-test/build.sh
+++ libraries/Runtime/BridJ/src/main/cpp/android-test/build.sh
@@ -10,27 +10,53 @@ ANDROID_PROJECT_HOME=`pwd`
 
 echo "BridJ version = $MVN_VERSION"
 
-if [[ ! -f "$BRIDJ_ANDROID_JAR" ]] ; then
-	echo "$BRIDJ_ANDROID_JAR is missing. Building BridJ first..."
+function buildBridJ {
 	cd $PROJECT_HOME
 	mvn package -DskipTests
-	cd $ANDROID_PROJECT_HOME 
+	cd $ANDROID_PROJECT_HOME
+}
+
+if [[ ! -f "$BRIDJ_ANDROID_JAR" ]] ; then
+	echo "$BRIDJ_ANDROID_JAR is missing. Building BridJ first..."
+	buildBridJ 
 	if [[ ! -f "$BRIDJ_ANDROID_JAR" ]] ; then
 		echo "Failed to build the Android JAR !" 
 		exit 1 ;
 	fi ;
 fi
 
-ndk-build -C .
-
 if [[ ! -f build.properties.template ]] ; then
 	echo "build.properties.template does not exist !"
 	exit 1 ;
 fi
 
+function helpQuit {
+	echo "#
+# List of valid build commands (can be combined) :
+#
+#      package		Build BridJ JAR (will be done implicitly if the JAR doesn't exist)
+#      emulator		Install into running emulator
+#      device		Install into USB-plugged device
+#      release		Build Release Android package
+#      debug		Build Debug Android package
+#      start			Start on emulator
+#
+# Default build commands = $DEFAULT_BUILD_CMDS
+# Typical debug commands = package emulator start
+#
+	" 
+	exit 1
+}
+
+DEFAULT_BUILD_CMDS="package release"
+if [[ "$*" == "" ]] ; then 
+	BUILD_CMDS=$DEFAULT_BUILD_CMDS ; 
+else
+	BUILD_CMDS=$* ;
+fi
+	
 cp -f $BRIDJ_ANDROID_JAR lib
 
-rm build.properties
 cat build.properties.template > build.properties
 
 if [[  -z "$ANDROID_SDK_HOME" ]] ; then
@@ -45,23 +71,39 @@ fi
 
 APK_FILE=bin/TouchExampleActivity-release.apk
 
-case $1 in
-	install)
-		ant install 
-		rm build.properties
-		;;
-	install-device)
-		ant install
-		rm build.properties
-		adb -d install $APK_FILE
-		;;
-	debug)
-		ant debug
-		rm build.properties
-		;;
-	*)
-		ant release
-		rm build.properties 
-		;;
-esac
+function compile {
+	ndk-build -C .
+	ant $*
+	rm build.properties
+}
 
+echo "# Provided build commands = $BUILD_CMDS"
+
+for C in $BUILD_CMDS ; do
+	case $C in
+		package)
+			buildBridJ
+			;;
+		emulator)
+			compile install
+			;;
+		device)
+			compile debug
+			adb -d install $APK_FILE
+			;;
+		debug)
+			compile debug
+			;;
+		release)
+			compile release
+			;;
+		start)
+			adb shell am start -a android.intent.action.MAIN -n com.example.hellojni/com.example.hellojni.HelloJni
+			;;
+		*)
+			helpQuit
+			;;
+	esac ;
+done
+
+echo "adb shell am start -a android.intent.action.MAIN -n com.example.hellojni/com.example.hellojni.HelloJni -W"
