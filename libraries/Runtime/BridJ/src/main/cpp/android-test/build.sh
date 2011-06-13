b9e5e6ff89474dd30e0bdeec7fa6d32c777c8021
Olivier Chafik <olivier.chafik@gmail.com>
2011-06-13 09:36:57 +0000

    BridJ: added bridj dependency to test Android project

--- libraries/Runtime/BridJ/src/main/cpp/android-test/build.sh
+++ libraries/Runtime/BridJ/src/main/cpp/android-test/build.sh
@@ -1,9 +1,20 @@
+#
+# See http://developer.android.com/guide/developing/building/building-cmdline.html
+#
+echo "You should ensure an emulator is running : android&"
+
+PROJECT_HOME=../../../..
+MVN_VERSION="`cat $PROJECT_HOME/pom.xml | grep '<version' | head -n 1 | sed -e 's/.*<version>\(.*\)<\/version>.*/\1/g'`" 
+echo "BridJ version = $MVN_VERSION"
+
 ndk-build -C .
 
 if [[ ! -f build.properties.template ]] ; then
 	echo "build.properties.template does not exist !"
 	exit 1 ;
 fi
+
+cp -f $PROJECT_HOME/target/bridj-$MVN_VERSION-android.jar lib
 
 rm build.properties
 cat build.properties.template > build.properties
@@ -18,7 +29,25 @@ if [[ ! -z "$KEYSTORE_PASS" ]] ; then
 	echo "key.store.password=$KEYSTORE_PASS" >> build.properties ;
 fi
 
-ant release 
+APK_FILE=bin/TouchExampleActivity-release.apk
 
-rm build.properties
+case $1 in
+	install)
+		ant install 
+		rm build.properties
+		;;
+	install-device)
+		ant install
+		rm build.properties
+		adb -d install $APK_FILE
+		;;
+	debug)
+		ant debug
+		rm build.properties
+		;;
+	*)
+		ant release
+		rm build.properties 
+		;;
+esac
 
