4d458b9b45661f7d5a3d7be4002df87a9f3227e6
Olivier Chafik <olivier.chafik@gmail.com>
2011-07-08 08:37:10 +0000

    ScalaCL: test array capture

--- libraries/OpenCL/ScalaCLPlugin/src/test/scala/scalacl/test/OpenCLTest.scala
+++ libraries/OpenCL/ScalaCLPlugin/src/test/scala/scalacl/test/OpenCLTest.scala
@@ -29,7 +29,7 @@ class OpenCLTest extends TestUtils {
       """
     ).newInstance()().asInstanceOf[Array[Float]]
     val a = 
-      (0 to 10).   map(i => cos(i).toFloat).toArray
+         (0 to 10).map(i => cos(i).toFloat).toArray
     
     assertArrayEquals(a, cla, 0.00001f)
   }
@@ -38,9 +38,7 @@ class OpenCLTest extends TestUtils {
   @Test
   def testScalarCapture {
     val cla = compileCodeWithPlugin(
-      stdImportsAndContext + """
-      
-      """,
+      stdImportsAndContext,
       """
       val n = (0 to 10).sum
       (0 to 10).cl.map(i => i * n + n * n * (i - 1)).toArray
@@ -49,8 +47,25 @@ class OpenCLTest extends TestUtils {
     
     val n = (0 to 10).sum
     val a = 
-      (0 to 10).   map(i => i * n + n * n * (i - 1)).toArray
+         (0 to 10).map(i => i * n + n * n * (i - 1)).toArray
     
+    assertArrayEquals(a, cla)
+  }
+
+  @Test
+  def testArrayCapture {
+    val cla = compileCodeWithPlugin(
+      stdImportsAndContext,
+      """
+      val arr = (0 to 10).toCLArray
+      (0 to 10).cl.map(i => arr(i / 2) * 2).toArray
+      """
+    ).newInstance()().asInstanceOf[Array[Int]]
+    
+    val arr = (0 to 10).toArray
+    val a = 
+         (0 to 10).map(i => arr(i / 2) * 2).toArray
+      
     assertArrayEquals(a, cla)
   }
 
