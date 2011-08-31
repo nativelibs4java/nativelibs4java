dac0238161fa1c22768f6aaaf818206b4f50609b
Olivier Chafik <olivier.chafik@gmail.com>
2011-08-31 20:14:57 +0200

    ScalaCL: moved to Scala 2.9.1 !

--- libraries/OpenCL/ScalaCL/build.sbt
+++ libraries/OpenCL/ScalaCL/build.sbt
@@ -6,7 +6,7 @@ organization := "com.nativelibs4java"
 
 //scalaHome := Some(file("/Users/ochafik/bin/scala-2.10.0.r25286-b20110715023800"))
 
-scalaVersion := "2.9.0"
+scalaVersion := "2.9.1"
 
 resolvers += "NativeLibs4Java Repository" at "http://nativelibs4java.sourceforge.net/maven/"
 
--- libraries/Runtime/BridJ/admin/setup_linux_buildFarm.sh
+++ libraries/Runtime/BridJ/admin/setup_linux_buildFarm.sh
@@ -1,4 +1,4 @@
-sudo apt-get install subversion git
+sudo apt-get install subversion git maven2
 
 mkdir src
 mkdir bin
@@ -14,16 +14,19 @@ echo "export DYNCALL_HOME=\"`pwd`\"" >> ~/.bashrc
 cat ../nativelibs4java/libraries/Runtime/BridJ/src/main/cpp/bridj/dyncall.diff | sed 's/~\/src\/dyncall\///' | patch -p0
 
 cd
-cd bin
-
-export MAVEN_VERSION=2.2.1
-wget ftp://ftp.inria.fr/pub/Apache//maven/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
-tar zxvf apache-maven-$MAVEN_VERSION-bin.tar.gz
-echo "export PATH=`pwd`/apache-maven-$MAVEN_VERSION/bin:\$PATH" >> ~/.bashrc
-chmod +x apache-maven-$MAVEN_VERSION/bin/*
-
-cd
-bash
-
 cd src/nativelibs4java/libraries
 mvn install -DskipTests
+
+#cd
+#cd bin
+#
+#export MAVEN_VERSION=2.2.1
+#wget ftp://ftp.inria.fr/pub/Apache//maven/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
+#tar zxvf apache-maven-$MAVEN_VERSION-bin.tar.gz
+#echo "export PATH=`pwd`/apache-maven-$MAVEN_VERSION/bin:\$PATH" >> ~/.bashrc
+#chmod +x apache-maven-$MAVEN_VERSION/bin/*
+
+#cd
+#bash
+
+
