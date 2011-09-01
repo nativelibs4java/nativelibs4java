2226a67d98041c72a823e72bb004af2ff8f0df14
Olivier Chafik <olivier.chafik@gmail.com>
2011-09-02 00:49:29 +0200

    BridJ: updated Windows binaries

--- libraries/Runtime/BridJ/src/main/cpp/bridj/org_bridj_Platform.h
+++ libraries/Runtime/BridJ/src/main/cpp/bridj/org_bridj_Platform.h
@@ -7,54 +7,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-/*
- * Class:     org_bridj_Platform
- * Method:    sizeOf_size_t
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_org_bridj_Platform_sizeOf_1size_1t
-  (JNIEnv *, jclass);
-
-/*
- * Class:     org_bridj_Platform
- * Method:    sizeOf_time_t
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_org_bridj_Platform_sizeOf_1time_1t
-  (JNIEnv *, jclass);
-
-/*
- * Class:     org_bridj_Platform
- * Method:    sizeOf_wchar_t
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_org_bridj_Platform_sizeOf_1wchar_1t
-  (JNIEnv *, jclass);
-
-/*
- * Class:     org_bridj_Platform
- * Method:    sizeOf_ptrdiff_t
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_org_bridj_Platform_sizeOf_1ptrdiff_1t
-  (JNIEnv *, jclass);
-
-/*
- * Class:     org_bridj_Platform
- * Method:    sizeOf_long
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_org_bridj_Platform_sizeOf_1long
-  (JNIEnv *, jclass);
-
-/*
- * Class:     org_bridj_Platform
- * Method:    getMaxDirectMappingArgCount
- * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_org_bridj_Platform_getMaxDirectMappingArgCount
-  (JNIEnv *, jclass);
-
 #ifdef __cplusplus
 }
 #endif
