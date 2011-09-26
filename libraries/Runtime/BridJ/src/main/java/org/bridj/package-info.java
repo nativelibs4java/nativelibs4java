4caf521e85ea1b29c5961d563fb10b266eff194a
Olivier Chafik <olivier.chafik@gmail.com>
2011-09-26 22:18:44 +0100

    BridJ: updated CHANGELOG

--- libraries/Runtime/BridJ/src/main/java/org/bridj/package-info.java
+++ libraries/Runtime/BridJ/src/main/java/org/bridj/package-info.java
@@ -1,5 +1,5 @@
 /**
-BridJ core classes and C runtime ({@link org.bridj.Pointer}, {@link org.bridj.BridJ}, {@link org.bridj.SizeT}, {@link org.bridj.Platform}...).
+BridJ core classes and C runtime ({@link org.bridj.Pointer}, {@link org.bridj.BridJ}, {@link org.bridj.SizeT}, {@link org.bridj.Platform}, {@link org.bridj.LastError}...).
 <p>
 See <a href="http://code.google.com/p/bridj/wiki/FAQ">BridJ's wiki</a> for more information.
 */
