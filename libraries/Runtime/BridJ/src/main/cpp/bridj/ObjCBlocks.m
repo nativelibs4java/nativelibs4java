c7aa7bbd1c3379ced1bb7c6c41b91b07e85a7b9c
Olivier Chafik <olivier.chafik@gmail.com>
2011-11-05 23:07:37 +0000

    BridJ: cleanup of Objective-C blocks hack (issue #192)

--- libraries/Runtime/BridJ/src/main/cpp/bridj/ObjCBlocks.m
+++ libraries/Runtime/BridJ/src/main/cpp/bridj/ObjCBlocks.m
@@ -5,28 +5,16 @@
 #include <Block.h>
 #include "bridj.hpp"
 
-//http://cocoawithlove.com/2009/10/how-blocks-are-implemented-and.html
+/*
+http://cocoawithlove.com/2009/10/how-blocks-are-implemented-and.html
+http://www.opensource.apple.com/source/libclosure/libclosure-38/BlockImplementation.txt
+*/
 typedef struct Block_literal {
-    void *isa;
- 
-    int flags;
-    int reserved; // is actually the retain count of heap allocated blocks
- 
-    void (*invoke)(void *, ...); // a pointer to the block's compiled code
- 
-    struct Block_descriptor {
-        unsigned long int reserved; // always nil
-        unsigned long int size; // size of the entire Block_literal
-         
-        // functions used to copy and dispose of the block (if needed)
-        void (*copy_helper)(void *dst, void *src);
-        void (*dispose_helper)(void *src); 
-    } *descriptor;
- 
-    // Here the struct contains one entry for every surrounding scope variable.
-    // For non-pointers, these entries are the actual const values of the variables.
-    // For pointers, there are a range of possibilities (__block pointer,
-    // object pointer, weak pointer, ordinary pointer)
+	void* isa;
+	int flags;
+	int reserved; 
+	void (*invoke)(void *, ...);
+	void* descriptor;
 } Block_literal;	
 	
 const void* createObjCBlock() {
