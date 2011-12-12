5f915df1f3da1378e8d5bfbf3282ae32af60f66c
Olivier Chafik <olivier.chafik@gmail.com>
2011-12-12 23:05:04 +0000

    BridJ: added ProcessUtils.getCurrentProcessId()

--- libraries/Runtime/BridJ/src/main/java/org/bridj/util/ProcessUtils.java
+++ libraries/Runtime/BridJ/src/main/java/org/bridj/util/ProcessUtils.java
@@ -4,19 +4,46 @@
  */
 package org.bridj.util;
 
+import org.bridj.ann.Library;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bridj.BridJ;
+import org.bridj.Platform;
+import org.bridj.ann.Convention;
 import static java.lang.System.getProperty;
 
 /**
- * Util methods to launch processes, including JVM processes
+ * Util methods to : query the process id from the current process, launch processes (including JVM processes)
  * @author Olivier
  */
 public class ProcessUtils {
 
+    @Library("kernel32")
+    @Convention(Convention.Style.StdCall)
+    static class Kernel32 {
+        public static native int GetCurrentProcessId();
+    }
+    @Library("c")
+    static class LibC {
+        public static native int getpid();
+    }
+    
+    /**
+     * Get the current process native id
+     */
+    public static int getCurrentProcessId() {
+        if (Platform.isWindows()) {
+            BridJ.register(Kernel32.class);
+            return Kernel32.GetCurrentProcessId();
+        } else {
+            BridJ.register(LibC.class);
+            return LibC.getpid();
+        }
+    }
+    
     public static String[] computeJavaProcessArgs(Class<?> mainClass, List<?> mainArgs) {
         List<String> args = new ArrayList<String>();
         args.add(new File(new File(getProperty("java.home")), "bin" + File.separator + "java").toString());
