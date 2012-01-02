ed83bb2ddc33093937f89992746c815356b8e78c
Olivier Chafik <olivier.chafik@gmail.com>
2012-01-02 17:41:12 +0000

    cleanup

--- libraries/Scalace/tmpTestClassesOptimized/issue44_capturingAVarInClosuresThatEvaluateLater.scala
+++
@@ -1,9 +0,0 @@
-package tests
-class Test_issue44_capturingAVarInClosuresThatEvaluateLater_Optimized_9(n: Int) {
-      
-      def issue44_capturingAVarInClosuresThatEvaluateLater() = {
-      
-      (1 to 10).map {i => j: Int => i + j}.map(_(10))
-    
-      }
-    }
