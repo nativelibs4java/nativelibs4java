3eadb461debf00c5d53b2a872603c87171ae7f6d
Olivier Chafik <olivier.chafik@gmail.com>
2012-04-13 08:41:57 +0100

    JavaCL: tweaked apple header

--- libraries/OpenCL/OpenCL4Java/src/main/headers/1.2/CL/cl_apple.h
+++ libraries/OpenCL/OpenCL4Java/src/main/headers/1.2/CL/cl_apple.h
@@ -1,3 +1,4 @@
+
 /* Context GL sharing
  *
  * Please check for the "cl_APPLE_gl_sharing" extension using clGetDeviceInfo(CL_DEVICE_EXTENSIONS)
@@ -20,5 +21,40 @@
  *
  * NOTE:  Make sure that appropriate cl_gl.h header file is included separately
  */
-#define CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE        0x10000000 /* Introduced in Mac OS /* Error code returned by clGetGLContextInfoAPPLE if an invalid platform_gl_ctx is provided           */
-#define CL_INVALID_GL_CONTEXT_APPLE                         -1000      /* Introduced in Mac OS 
+#define CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE        0x10000000 /* Introduced in Mac OS X 10.6 */
+
+/* Apple extension for retrieving OpenGL context information for a CL context 
+ * created via the above method.
+ *
+ * Provides a query mechanism to retrieve OpenGL context specific information 
+ * from an OpenCL context to help identify device specific mappings and usage.
+ * 
+ * For example, one possible usage would be to allow the client to map a CGL 
+ * virtual screen index to an appropriate CL device id to insure that the 
+ * rendering device and the compute device are the same, thus guaranteeing 
+ * any shared OpenGL memory that is attached o a CL memory object remains 
+ * resident on the active device.
+ */
+cl_int	clGetGLContextInfoAPPLE ( cl_context /* context */,
+								  void * /* platform_gl_ctx */,
+								  cl_gl_platform_info /* param_name */,
+								  size_t /* param_value_size */,
+								  void * /* param_value */,
+								  size_t * /* param_value_size_ret */)  ;  
+
+/* The list of supported param_name values and the information returned in param_value by 
+ * clGetContextInfo are listed below:
+
+ * Returns a cl_device_id for the CL device associated with the virtual screen for 
+ * the given CGL context.  Return type: cl_device_id
+ */
+#define CL_CGL_DEVICE_FOR_CURRENT_VIRTUAL_SCREEN_APPLE 		0x10000002 /* Introduced in Mac OS X 10.6 */
+
+/* Returns an array of cl_device_ids for the CL device(s) corresponding to 
+ * the virtual screen(s) for the given CGL context.   Return type: cl_device_id[]
+ */
+#define CL_CGL_DEVICES_FOR_SUPPORTED_VIRTUAL_SCREENS_APPLE	0x10000003 /* Introduced in Mac OS X 10.6 */
+
+/* Error code returned by clGetGLContextInfoAPPLE if an invalid platform_gl_ctx is provided           */
+#define CL_INVALID_GL_CONTEXT_APPLE                         -1000      /* Introduced in Mac OS X 10.6 */
+
