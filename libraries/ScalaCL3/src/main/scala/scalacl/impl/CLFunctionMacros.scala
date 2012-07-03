2657f85b32599839f35cdf263e77ff16b5f7edb0
Olivier Chafik <olivier.chafik@gmail.com>
2012-07-03 02:28:59 +0100

    ScalaCLv3: fix macros

--- libraries/ScalaCL3/src/main/scala/scalacl/impl/CLFunctionMacros.scala
+++ libraries/ScalaCL3/src/main/scala/scalacl/impl/CLFunctionMacros.scala
@@ -11,7 +11,7 @@ private[impl] object CLFunctionMacros
   /// These ids are not necessarily unique, but their values should be dispersed well
   private def nextKernelId = random.nextLong
   
-  private[impl] def convertFunction[T, U](c: Context)(f: c.Expr[T => U]): c.Expr[CLFunction[T, U]] = {
+  private[impl] def convertFunction[T: c.TypeTag, U: c.TypeTag](c: Context)(f: c.Expr[T => U]): c.Expr[CLFunction[T, U]] = {
     import c.universe._
     
     val Function(List(param), body) = f.tree
@@ -29,7 +29,7 @@ private[impl] object CLFunctionMacros
 	    capturedOutputs = Seq(),
 	    capturedConstants = Seq()
     )
-    val src = c.Expr(Literal(Constant(code)))
+    val src = c.Expr[String](Literal(Constant(code)))
     val id = c.Expr[Long](Literal(Constant(nextKernelId)))
     c.reify {
       new CLFunction[T, U](f.splice, new Kernel(id.splice, src.splice))
