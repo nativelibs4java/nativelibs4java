ab819c411a7baef328597bfd7f09f14553a5c3de
Olivier Chafik <olivier.chafik@gmail.com>
2012-09-23 19:17:16 +0100

    JavaCL: javadoc++

--- libraries/OpenCL/Core/src/main/velocity/com/nativelibs4java/opencl/package-info.java
+++ libraries/OpenCL/Core/src/main/velocity/com/nativelibs4java/opencl/package-info.java
@@ -1,6 +1,6 @@
 #parse("main/Header.vm")
 /**
- * Object-oriented wrappers around the OpenCL API, with methods that help creating and managing OpenCL applications.<br/>
+ * Object-oriented wrappers around the OpenCL API ({@link com.nativelibs4java.opencl.JavaCL}, {@link com.nativelibs4java.opencl.CLPlatform}, {@link com.nativelibs4java.opencl.CLContext}, {@link com.nativelibs4java.opencl.CLProgram}, {@link com.nativelibs4java.opencl.CLKernel}, {@link com.nativelibs4java.opencl.CLBuffer}...)<br>
  * Also see the low-level bindings : {@link com.nativelibs4java.opencl.library.OpenCL4Java}
  */
 package com.nativelibs4java.opencl;
