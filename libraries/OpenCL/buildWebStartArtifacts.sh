5a6cc02360f4d2cd3de7d359a0d1d5152417b1dc
Olivier Chafik <olivier.chafik@gmail.com>
2012-10-10 00:19:07 +0100

    JavaCL: added webstart build script

---
+++ libraries/OpenCL/buildWebStartArtifacts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mvn -Pwebstart -Dstorepass=$KEYSTORE_PASS -DskipTests clean install || exit 1
+
+rm -fR target-webstart
+mkdir target-webstart
+cd target-webstart
+
+for SUB in JavaCL Demos OpenGLDemos ; do
+	cp ../$SUB/target/*-shaded.jar . || exit 1
+done
+
+open .
+open /Applications/Cyberduck.app/ || exit 1
+
