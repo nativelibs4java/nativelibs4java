476aec7f695420fec43e6e543f7ac431093babca
Olivier Chafik <olivier.chafik@gmail.com>
2014-04-21 18:50:51 +0200

    JavaCL Generator: add support for 3-components vectors (issue #497)

--- libraries/OpenCL/JavaCL/src/test/opencl/com/nativelibs4java/opencl/generator/Structs.c
+++ libraries/OpenCL/JavaCL/src/test/opencl/com/nativelibs4java/opencl/generator/Structs.c
@@ -6,3 +6,9 @@ kernel void f(__global S *pS) {
   pS->a = pS->a * 2 + 10;
   pS->b = pS->b * 10 + 100;
 }
+
+kernel void g(float3 floats, __global float *out) {
+  out[0] = floats.x;
+  out[1] = floats.y;
+  out[2] = floats.z;
+}
